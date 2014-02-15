42143bfcdee79e8be34e2f087b01aef41e9f0aee
root <root@infederal.com>
2014-02-15 18:14:43 -0500

    new errors

--- handstack/settings.py
+++ handstack/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +59,11 @@ WSGI_APPLICATION = 'handstack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'actup1',
+        'USER': 'actup1',
+        'PASSWORD': '298northampton',
+        'HOST': '127.0.0.1',
     }
 }
 
